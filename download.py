a4fbf889e93e900266338697f6db3cfc942965a2
Altynai <Altynai.me@gmail.com>
2013-03-22 13:23:53 +0800

    add large size

--- download.py
+++ download.py
@@ -7,8 +7,9 @@ import time
 from BeautifulSoup import BeautifulSoup
 
 defaulterror = "Ooooops!\nPlease Check Arguments.\npython download.py url|id.\n"
+firstphotourl = None
 
-def download_photo(photoURL, savepath='.' + os.sep + 'default'):
+def download_photo(photoURL, savepath = '.' + os.sep + 'default'):
     """
     Download the photo by the 'photoURL',
     save it into 'savepath'
@@ -22,6 +23,12 @@ def download_photo(photoURL, savepath='.' + os.sep + 'default'):
     with open(savepath, 'wb') as stream:
         stream.write(photostream.read())
 
+def debug(html, filename = 'temp.html'):
+    """
+    Write 'html' into the file
+    """
+    with open(filename, 'w') as filewriter:
+        filewriter.write(html)
 
 def get_html(URL):
     """
@@ -31,6 +38,19 @@ def get_html(URL):
     html = htmlreader.read()
     return html
 
+def get_first_photo(albumURL):
+    """
+    When the size is `large`, select the first photo and then catch the others one by one.
+    """
+    html = get_html(albumURL)
+    soup = BeautifulSoup(html)
+    first = str(soup.findAll('a', {'class':"photolst_photo"})[0])
+    regex = re.compile("href=\"(.*?)\"")
+    first = regex.findall(first)[0]
+    global firstphotourl
+    firstphotourl = first
+    return first
+
 def get_albumname():
     """
     Get album's name that use for the directory's name(current time)
@@ -38,22 +58,35 @@ def get_albumname():
     albumname = time.strftime("%H_%M_%S", time.localtime())
     return albumname
 
-def parse_html(html):
+def parse_html(html, size):
     """
-    Get next page and every photo's url
+    Get next page and every photo's url, depending on its size
     """
-
+    global firstphotourl
     soup = BeautifulSoup(html)
-    nextURL = str(soup.findAll(attrs={"class": "next"}))
-    regex = re.compile("href=\"(.*)\"")
-    nextURL = regex.findall(nextURL)
+    if size == "small":
+        nextURL = str(soup.findAll(attrs={"class": "next"}))
+        regex = re.compile("href=\"(.*?)\"")
+        nextURL = regex.findall(nextURL)
     
-    if(len(nextURL) != 0):
-        nextURL = nextURL[0]
+        if(len(nextURL) != 0):
+            nextURL = nextURL[0]
+        else:
+            nextURL = None
     else:
-        nextURL = None
+        nextURL = str(soup.findAll('link', {"rel": "next"}))
+        regex = re.compile("href=\"(.*?)\"")
+        nextURL = regex.findall(nextURL)[0]
+        if nextURL == firstphotourl:
+            nextURL = None
+
     photolist = []
-    for photoitem in soup.findAll(attrs={"class": "photo_wrap"}):
+    if size == "small":
+        classvalue = "photo_wrap"
+    else:
+        classvalue = "mainphoto"
+
+    for photoitem in soup.findAll(attrs={"class": classvalue}):
         photoitem = str(photoitem)
         regex = re.compile("img src=\"(.*)\"")
         photoURL = regex.findall(photoitem)[0]
@@ -62,7 +95,7 @@ def parse_html(html):
 
 def error_report(error):
     """
-    Some argument or URL maybe wrong and report an error.
+    Some arguments or URL maybe wrong and report an error.
     """
     sys.stderr.write(error)
     sys.exit(1)
@@ -84,7 +117,10 @@ def check_URL(albumURL):
         error_report("the album '%s' does not exist." % albumURL)
     return albumURL
 
-def main(albumURL):
+
+def main(albumURL, size):
+    if size == "large":
+        albumURL = get_first_photo(albumURL)
     html = get_html(albumURL)
     albumname = get_albumname()
     savepath = '.' + os.sep + albumname
@@ -92,9 +128,9 @@ def main(albumURL):
     allphotolist = []
 
     while True:
-        nextURL, photolist = parse_html(html)
+        nextURL, photolist = parse_html(html, size)
         for photoURL in photolist:
-        	allphotolist.append(photoURL)
+            allphotolist.append(photoURL)
         if(nextURL == None):
             break
         html = get_html(nextURL)
@@ -103,11 +139,19 @@ def main(albumURL):
         download_photo(photoURL, savepath)
         print count, '/', total, '\tDone\n'
         count += 1
-        time.sleep(0.8)
+        time.sleep(0.2)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    argc = len(sys.argv)
+    if argc != 2 and argc!= 3:
         error_report(defaulterror)
+    size = "small"
+
+    if argc == 3:
+        size = sys.argv[2].lower()
+        if size != "small" and size != "large":
+            error_report("Unknown argument '%s'\n" % size)
+
     albumURL = sys.argv[1]
     albumURL = check_URL(albumURL)
-    main(albumURL)
+    main(albumURL, size)
